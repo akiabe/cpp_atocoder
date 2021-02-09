c30f124f85c213d7f73a33d2d8dc2a1901ee8381
Akifumi Abe <akiabe0831@gmail.com>
2021-02-10 05:00:27 +0900

    Initial Commit

---
+++ cpp_atocoder/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_atocoder
+//
+//  Created by 阿部 昌史 on 2021/02/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
