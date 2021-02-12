1e816c1109ca66e6d51968db4726054f566c02f5
Akifumi Abe <akiabe0831@gmail.com>
2021-02-12 18:13:55 +0900

    test

--- cpp_atocoder/test.cpp
+++ cpp_atocoder/test.cpp
@@ -1,17 +1,15 @@
 //
-//  main.cpp
+//  test.cpp
 //  cpp_atocoder
 //
 //  Created by 阿部 昌史 on 2021/02/10.
 //
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-    
-    int a1, a2, a3;
-    cin >> a1
-    
+    cout << "Hello" << endl;
+    cout << "Hello" << endl;
+    cout << "Hello" << endl;
 }
---
+++ cpp_atocoder/test.hpp
@@ -0,0 +1,13 @@
+//
+//  test.hpp
+//  cpp_atocoder
+//
+//  Created by 阿部 昌史 on 2021/02/10.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
